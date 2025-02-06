a2590469e5ae2ca23ffe7eb88fb826ed891b5349
robbinl <robbinl@telusplanet.net>
2025-02-06 16:47:06 -0700

    sw_led_partial

---
+++ embedded-systems/section3/topic3/assets/sw_led_partial.c
@@ -0,0 +1,79 @@
+// Robbin Law 2025-01-26
+
+#include "sw_led.h"
+#include "derivative.h"
+
+int SWL_Pushed(SWL_SwitchPos pos, DebounceOption deb){
+  // Set the samples to be different so the while
+  // loop will be entered into the first time.
+  unsigned char cSample1 = 1; 
+  unsigned char cSample2 = 0;
+  if(deb == SWL_DebOff){
+    // Sample all switches only once
+    cSample1 = PT1AD1 & 0b00011111;
+  }
+  else{
+    // Only when both samples of the switches are equal
+    // before and after a short delay then move on.
+    // It is at that time that the switch is stable
+    // and not bouncing anymore.
+    while (cSample1 != cSample2){
+      // Sample all switches
+      cSample1 = PT1AD1 & 0b00011111;
+      // Blocking delay for 10 ms
+      Delay_ms(10);
+      // Sample all switches again
+      cSample2 = PT1AD1 & 0b00011111;
+    }
+  }  
+  // Check the switch/s of interest.
+  if(cSample1 & pos)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+SwState Sw_State(SwState* state, SWL_SwitchPos pos){
+  if(SWL_Pushed(pos, SWL_DebOn))
+  {
+    if((*state == Idle)||(*state == Released))
+    {
+      *state = Pressed;
+    }
+    else
+    {
+      *state = Held;
+    }
+  }
+  else
+  {
+    if((*state == Pressed)||(*state == Held))
+    {
+      *state = Released;
+    }
+    else
+    {
+      *state = Idle;
+    }
+  }
+  return *state;
+}
+
+void Delay_ms(unsigned int delay)
+{
+  unsigned int loopCount;
+  // Predecrement the delay variable then check,
+  // if it is anything but 0 enter the loop,
+  // if it is 0 break out of the while loop.  
+  while(--delay){
+    // Calculated value for 1 ms via AD2 oscilloscope is 2665 decimal.
+    // Value adjusted, after considering JSR cycles and other overhead to 2659 decimal.
+    // This value of 2659 is as close to a 1 ms delay as can be measured.
+    loopCount = 2659;
+    // Predecrement the loopCount variable then check,
+    // if it is anything but 0 loop again,
+    // if it is 0 break out of the while loop.  
+    while(--loopCount);      
+  }
+}
