393d5dd686940f5aef76e7516f2b9e75b7abb2f4
robbinl <robbinl@telusplanet.net>
2025-03-03 07:11:58 -0700

    r

--- docs/embedded-systems/section5/topic1/assets/rti.h
+++ docs/embedded-systems/section5/topic1/assets/rti.h
@@ -1,27 +1,11 @@
-//RTI Module Library
-//File: rti.h (header file)
-//Processor: MC9S12XDP512
-//Crystal: 16 MHz
-//by Carlos Estay
-//September 2023
-//Last edit, October 24th, 2023
+// Real Time Interrupt
+// Robbin Law
+// 2025-03-03
 
-
-//This has to be declared as a global variable in rti.c
+// This variable has to be declared as 
+// a global variable in rti.c (without the extern keyword)
 extern volatile unsigned long rtiMasterCount;
 
-/// @brief Enables RTI Moule
-/// @param  
 void RTI_Init(void);
+void RTI_Delay_ms(unsigned long delay);
 
-/// @brief Enables RTI Module with callback to be used in main
-/// @param function 
-/*
-We will implement this function later once we cover 
-function pointers
-*/
-void RTI_InitCallback(void(*function)(void));
-
-/// @brief Blocking delay to be used  once the RTI MOdule is enabled
-/// @param timeout 
-void RTI_Delay_ms(unsigned int ms);
