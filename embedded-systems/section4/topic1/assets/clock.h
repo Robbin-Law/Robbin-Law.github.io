4787bc916ad50620a19d12e3d347993811e6fe62
robbinl <robbinl@telusplanet.net>
2025-03-03 07:08:59 -0700

    rti

--- embedded-systems/section4/topic1/assets/clock.h
+++ embedded-systems/section4/topic1/assets/clock.h
@@ -1,19 +1,12 @@
 // Clock-System
 // Robbin Law
-// 2025-02-25
+// 2025-02-27
 
 #define DEF_BUS_CLOCK 8000000
-
-typedef enum ClockDivTypedef
-{
-    ClockOutDiv1 = 0b00000000,
-    ClockOutDiv2 = 0b00000001,
-    ClockOutDiv3 = 0b00000010,
-    ClockOutDiv4 = 0b00000011
-} ClockOutDiv;
 
 void Clock_Set2MHZ(void);
 void Clock_Set8MHZ(void);
 void Clock_Set20MHZ(void);
-void Clock_EnableOutput(ClockOutDiv div);
+void Clock_Set40MHZ(void);
+void Clock_EnableOutput(void);
 unsigned long Clock_GetBusSpeed(void);
