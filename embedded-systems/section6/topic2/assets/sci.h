940a6592ff4a871938f19bb2968aa0441bd8df2c
robbinl <robbinl@telusplanet.net>
2025-03-12 12:29:14 -0600

    ica8

---
+++ embedded-systems/section6/topic2/assets/sci.h
@@ -0,0 +1,10 @@
+// Serial Communications Interface
+// Robbin Law
+// 2025-03-08
+
+#define BUFFER_SIZE 64
+
+void SCI_Init(void);
+void sci0_txByte (unsigned char byte);
+void sci0_txStr (char const* strIndex);
+unsigned char sci0_rxByte(unsigned char* pRxByte);
