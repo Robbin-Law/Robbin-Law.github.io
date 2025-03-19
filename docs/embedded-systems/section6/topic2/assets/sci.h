0172a63ec90338934de684bd31443fbfbfebd38c
robbinl <robbinl@telusplanet.net>
2025-03-19 12:15:32 -0600

    r

--- docs/embedded-systems/section6/topic2/assets/sci.h
+++ docs/embedded-systems/section6/topic2/assets/sci.h
@@ -4,7 +4,7 @@
 
 #define BUFFER_SIZE 64
 
-void SCI_Init(void);
-void sci0_txByte (unsigned char byte);
+void SCI_Init(unsigned long ulBaudRate);
+void sci0_txByte (unsigned char txByte);
 void sci0_txStr (unsigned char* strIndex);
 unsigned char sci0_rxByte(unsigned char* pRxByte);
